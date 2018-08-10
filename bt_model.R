6634e675b8e60e0956cff78aff505e2bea22bc3e
a0082273 <kasugai.kanten@gmail.com>
2018-08-10 23:06:12 +0900

    only main.py

--- bt_model.R
+++ bt_model.R
@@ -1,14 +1,14 @@
 library("BradleyTerry2")
 
-data <- read.csv("torikumi_btmodel_data/201805_bt_all.csv")
-#data <- read.csv("torikumi_btmodel_data/201805_bt_rm_ketsujo.csv")
+#data <- read.csv("input/torikumi_btmodel_data/201801_bt_all.csv")
+data <- read.csv("input/torikumi_btmodel_data/201801_bt_rm_kyujo.csv")
 data[, 1] <- NULL
 
 bt_model <- BTm(
-  outcome = cbind(e_win, w_win), 
-  player1 = e_rikishi, player2 = w_rikishi, 
+  outcome = cbind(e_win, w_win),
+  player1 = e_rikishi, player2 = w_rikishi,
   data = data)
 BTabilities(bt_model)
 
-write.csv(BTabilities(bt_model), "torikumi_btmodel_data/201805_bt_ability_all.csv", quote=FALSE, row.names=TRUE)
-#write.csv(BTabilities(bt_model), "torikumi_btmodel_data/201805_bt_ability_rm_ketsujo.csv", quote=FALSE, row.names=TRUE)
+#write.csv(BTabilities(bt_model), "input/bt_ability/201801_bt_ability_all.csv", quote=FALSE, row.names=TRUE)
+write.csv(BTabilities(bt_model), "input/bt_ability/201801_bt_ability_rm_kyujo.csv", quote=FALSE, row.names=TRUE)
